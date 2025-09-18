d83fc089331e9992b74b13889ce4b96063317a1c
Atharva <atharvajofficial@email.com>
2025-09-18 20:08:17 +0530

    Atharva testing

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
-import { Router,Route,Routes } from 'react-router-dom'
+import { Router, Route, Routes } from 'react-router-dom'
+import { Button } from "@/components/ui/button"
 import './App.css'
 const App = () => {
   return (
     <Routes>
-      <Route path="/" element={<div className='text-xl text-red-500'>Home</div>} />
+      <Route path="/" element={<div className='text-xl text-red-500'>Home, Atharva testing-shadcn installed<Button onClick={() => { console.log("clicked") }}>Atharva testing</Button></div>} />
       <Route path="/about" element={<div className='text-xl text-red-600'>About</div>} />
     </Routes>
   )
