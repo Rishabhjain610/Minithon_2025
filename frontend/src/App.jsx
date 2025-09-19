24996270b1a90efbab60720ec5de6e5b0daa00b7
Rishabh Jain <jainrishabh2610@gmail.com>
2025-09-19 11:13:37 +0530

    loginadded

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,13 +5,10 @@ import { Button } from "./components/ui/button"
 import Footer from './components/Footer';
 import { Routes, Route, Navigate } from "react-router-dom";
 import Form from './components/Form';
+import Login from './pages/Login';
+import Signup from './pages/Signup';
 const App = () => {
-  // const { user, loading } = useContext(UserDataContext);
-
-  // // Optional: Show a loading spinner while fetching user data
-  // if (loading) {
-  //   return <div className="flex items-center justify-center h-screen">Loading...</div>;
-  // }
+ 
 
   return (
     <>
@@ -19,6 +16,8 @@ const App = () => {
       {/* <LandingPage></LandingPage> */}
       <Routes>
         <Route path="/" element={<LandingPage />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
         <Route path="/form" element={<Form />} />
       </Routes>
       <Footer></Footer>
