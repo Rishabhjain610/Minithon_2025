a25834b9132a32dd8f825470711746d3ca28ddec
Atharva <atharvajofficial@email.com>
2025-09-19 10:53:51 +0530

    Atharva

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -3,27 +3,15 @@ import Language from "./Language"
 
 export default function Navbar() {
   return (
-    <header className="bg-white border-b border-gray-100 px-6 py-4">
+    <header className="bg-white border-b border-gray-100 px-6 py-2">
       <div className="max-w-7xl mx-auto flex items-center justify-between">
         <div className="flex items-center space-x-2">
-          <div className="w-8 h-8 bg-black rounded flex items-center justify-center">
-            <div className="w-4 h-4 bg-white rounded-sm"></div>
-          </div>
-          <span className="text-xl font-bold text-black">REALTY</span>
+          <img src="/Roomy.png" className="h-16 w-30" alt="" />
         </div>
 
         <nav className="hidden md:flex items-center space-x-8">
           <a href="#" className="text-gray-700 hover:text-black">
-            Featured
-          </a>
-          <a href="#" className="text-gray-700 hover:text-black">
-            Buy
-          </a>
-          <a href="#" className="text-gray-700 hover:text-black">
-            Rent
-          </a>
-          <a href="#" className="text-gray-700 hover:text-black">
-            Sell
+            Home
           </a>
           <a href="#" className="text-gray-700 hover:text-black">
             About us
